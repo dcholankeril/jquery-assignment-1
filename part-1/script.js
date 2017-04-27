0eca6cf293499a7b0f71bc8e45ac6d0d11554c5d
Diana Cholankeril <dianac@id.iit.edu>
2017-04-26 22:13:54 -0500

    rays didn’t attach to sun and mountain cap and mountain didn’t show up

--- part-1/script.js
+++ part-1/script.js
@@ -17,7 +17,7 @@ $('.view').append('<div class="sun"></div>');
 // 6. Write a [`while`] loop to create 5 divs. The div classes will be "**ray-1**", "**ray-2**", "**ray-3**", "**ray-4**", and "**ray-5**". Append each of these divs to the previously created "sun" div from step 5.
 
 var t = 1;
-while (t < 6) {
+while (t <= 5) {
     $("<div>").addClass("ray-" + i).appendTo(".sun")
 }
 
@@ -25,6 +25,15 @@ while (t < 6) {
 
 $('.view').append('<div class="mountain"></div>');
 
-// 1. Create a `div` element with a class name of "**mountain-top**" and append it to the previously created "mountain" div from step 7.
+// 8. Create a `div` element with a class name of "**mountain-top**" and append it to the previously created "mountain" div from step 7.
 
 $('.mountain').append('<div class="mountain-top"></div>');
+
+
+// 1. Write a [`do...while`] loop to create 3 divs. The div classes will be "**mountain-cap-1**", "**mountain-cap-2**", and "**mountain-cap-3**". Append each of these divs to the previously created "mountain-top" div from step 8.
+
+var c = 1;
+do {
+    $("<div>").addClass("mountain-cap-" + c).appendTo(".mountain-top");
+} while (c <= 3);
+
