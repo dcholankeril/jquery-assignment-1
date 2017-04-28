00493eb9400f58f5c4d6851a64889f644981abbc
Diana Cholankeril <dianac@id.iit.edu>
2017-04-27 21:35:12 -0500

    figuring click event listener out

--- part-2/script.js
+++ part-2/script.js
@@ -2,18 +2,6 @@
 
 
 for (var i = 1; i <= 196; i++) {
-    var rest = i % 2;
-    switch(rest) {
-        case 0:
-            $("<div>").addClass("box-type-1").appendTo(document.body);
-            break;
-        case 1:
-            $("<div>").addClass("box-type-2").appendTo(document.body);
-            break;
-    }
-}
-
-for (var i = 1; i <= 196; i++) {
     var boxDiv = document.createElement("div");
     boxDiv.className = "box";
 
@@ -35,4 +23,13 @@ $(".box").click(function() {
     }
 });
 
+$( "box" ).click({
+  click: function() {
+    $( this ).toggleClass( "clicked" );
+  }, mouseenter: function() {
+    $( this ).addClass( "clicked" );
+  }, mouseleave: function() {
+    $( this ).removeClass( "clicked" );
+  }
+});
 
